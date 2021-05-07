450e8ca2f829a6ba72c71be77edf751109a2c259
longpatrick <long.patrick1997@gmail.com>
2021-05-07 12:24:14 -0500

    change models to have descriptive fields; migrated db

--- workouts/models.py
+++ workouts/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Workout(models.Model):
-    workout_name = models.CharField(max_length=300),
+    workout_name = models.CharField(max_length=300, default='my workout')
     date_created = models.DateTimeField('date created')
+    def was_created_recently(self):
+        return self.date_created >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.workout_name
 
 class Exercise(models.Model):
-    workout = models.ForeignKey(Workout, on_delete=models.CASCADE),
-    exercise_name = models.CharField(max_length=200),
-    num_sets = models.IntegerField(default=0),
-    num_reps = models.IntegerField(default=0),
-    rest_time = models.IntegerField(default=0),
-    weight = models.IntegerField(default=0),
+    workout = models.ForeignKey(Workout, default=1, on_delete=models.CASCADE)
+    exercise_name = models.CharField(max_length=200, default='my exercise')
+    num_sets = models.IntegerField(default=0)
+    num_reps = models.IntegerField(default=0)
+    rest_time_between_sets_in_seconds = models.IntegerField(default=0)
+    weight_in_pounds = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.exercise_name
