620bbe798759877477ce20a34505355390c62cce
longpatrick <long.patrick1997@gmail.com>
2021-05-08 18:52:05 -0500

    adjust settings to allow showmigrations to work

--- hp_lifestyle/settings.py
+++ hp_lifestyle/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ 
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-v26ysf+o9u9)u2aho1zd9*@c2(a0i*we83#4s9z=clapsj@)g*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
