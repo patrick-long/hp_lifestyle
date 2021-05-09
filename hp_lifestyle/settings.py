6f1aa355d822b79b9b5e798c64aadb95cad14669
longpatrick <long.patrick1997@gmail.com>
2021-05-08 19:10:07 -0500

    wip: debug settings.py

--- hp_lifestyle/settings.py
+++ hp_lifestyle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-v26ysf+o9u9)u2aho1zd9*@c2(a0i*we83#4s9z=clapsj@)g*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
